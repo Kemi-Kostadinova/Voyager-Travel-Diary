99d2963cc55abb061d221dfb63c4c9c60126bf59
Kemi-Kostadinova <158486040+Kemi-Kostadinova@users.noreply.github.com>
2024-07-17 13:32:40 +0300

    Add hero section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
+import Header from "./components/header/Header"
+import Hero from "./components/hero/Hero"
+
 function App() {
 
 	return (
 		<>
-			<h1 className="text-3xl font-bold underline">Hello</h1>
+			<Header />
+			<Hero />
 		</>
 	)
 }
