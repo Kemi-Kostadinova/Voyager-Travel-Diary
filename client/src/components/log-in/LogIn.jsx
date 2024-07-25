2c5eb01840673a780493a5420800bdd59f46a90f
Kemi-Kostadinova <158486040+Kemi-Kostadinova@users.noreply.github.com>
2024-07-25 12:22:20 +0300

    Implement login

--- client/src/components/log-in/LogIn.jsx
+++ client/src/components/log-in/LogIn.jsx
@@ -1,6 +1,25 @@
 import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+
+import { useForm } from "../../hooks/useForm"
+import { useLogin } from "../../hooks/useAuth"
 
 export default function LogIn() {
+    const login = useLogin();
+    const navigate = useNavigate();
+    
+    const { values, onChange, onSubmit } = useForm(
+        { email: "", password: "" },
+        async ({ email, password }) => {
+            try {
+                await login(email, password);
+                navigate('/discover')
+            } catch (err) {
+                console.log(err.message);
+            }
+        } 
+    );
+
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-44 lg:px-8">
@@ -11,12 +30,12 @@ export default function LogIn() {
                         className="mx-auto h-6 w-auto"
                     />
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        Sign in to your account
+                        Log in to your account
                     </h2>
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form action="#" method="POST" className="space-y-6">
+                    <form onSubmit={onSubmit} className="space-y-6">
                         <div>
                             <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                                 Email address
@@ -28,7 +47,9 @@ export default function LogIn() {
                                     type="email"
                                     required
                                     autoComplete="email"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    value={values.email}
+                                    onChange={onChange}
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#0CA9E8] sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -51,7 +72,9 @@ export default function LogIn() {
                                     type="password"
                                     required
                                     autoComplete="current-password"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    value={values.password}
+                                    onChange={onChange}
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#0CA9E8] sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -61,7 +84,7 @@ export default function LogIn() {
                                 type="submit"
                                 className="flex w-full justify-center rounded-md bg-[#0CA9E8] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#008bb5] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
-                                Sign in
+                                Log in
                             </button>
                         </div>
                     </form>
